f5941fb418a300f97ad2afe7d45412e8ec0ecd3f
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-29 16:53:16 -0300

    coords and instance

--- knn_test.go
+++ knn_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/TopoSimplify/node"
 	"github.com/TopoSimplify/pln"
 	"github.com/TopoSimplify/rng"
-	"github.com/TopoSimplify/dp"
 	"github.com/TopoSimplify/hdb"
 	"github.com/intdxdt/mbr"
+	"github.com/TopoSimplify/common"
+	"github.com/intdxdt/iter"
+	"github.com/TopoSimplify/lnr"
 )
 
 type iG struct{ g geom.Geometry }
@@ -19,22 +21,23 @@ func (o *iG) Geometry() geom.Geometry {
 	return o.g
 }
 
-func linearCoords(wkt string) []geom.Point {
-	return geom.NewLineStringFromWKT(wkt).Coordinates()
+func linearCoords(wkt string) geom.Coords {
+	return geom.NewLineStringFromWKT(wkt).Coordinates
 }
 
-func createNodes(indxs [][]int, coords []geom.Point) []*node.Node {
+func createNodes(id *iter.Igen, indxs [][]int, coords geom.Coords, instance lnr.Linegen) []node.Node {
 	var poly = pln.CreatePolyline(coords)
-	var hulls = make([]*node.Node, 0, len(indxs))
+	var hulls = make([]node.Node, 0, len(indxs))
 	for i := range indxs {
 		var r = rng.Range(indxs[i][0], indxs[i][1])
-		hulls = append(hulls, node.CreateNode(poly.SubCoordinates(r), r, dp.NodeGeometry))
+		hulls = append(hulls, node.CreateNode(id, poly.SubCoordinates(r), r, common.Geometry, instance))
 	}
 	return hulls
 }
 
 func TestDB(t *testing.T) {
-	var g    = goblin.Goblin(t)
+	var g = goblin.Goblin(t)
+	var id = iter.NewIgen()
 	var wkts = []string{
 		"POINT ( 190 310 )", "POINT ( 220 400 )", "POINT ( 260 200 )", "POINT ( 260 340 )",
 		"POINT ( 260 290 )", "POINT ( 310 280 )", "POINT ( 350 250 )", "POINT ( 350 330 )",
@@ -51,10 +54,10 @@ func TestDB(t *testing.T) {
 		}
 
 		g.It("should test k nearest neighbour", func() {
-			var objects = make([]*node.Node, 0)
+			var objects = make([]node.Node, 0)
 			for i := range wkts {
 				var g = geom.ReadGeometry(wkts[i])
-				objects = append(objects, &node.Node{Id: i, MBR: g.Bounds(), Geom: g})
+				objects = append(objects, node.Node{Id: i, MBR: g.Bounds(), Geom: g})
 			}
 			var tree = hdb.NewHdb()
 			tree.Load(objects)
@@ -70,16 +73,14 @@ func TestDB(t *testing.T) {
 			g.Timeout(1 * time.Hour)
 
 			var coords = linearCoords("LINESTRING ( 780 600, 740 620, 720 660, 720 700, 760 740, 820 760, 860 740, 880 720, 900 700, 880 660, 840 680, 820 700, 800 720, 760 700, 780 660, 820 640, 840 620, 860 580, 880 620, 820 660 )")
-			var hulls = createNodes([][]int{{0, 3}, {3, 8}, {8, 13}, {13, 17}, {17, len(coords) - 1}}, coords)
+			var hulls = createNodes(id, [][]int{{0, 3}, {3, 8}, {8, 13}, {13, 17}, {17, coords.Len() - 1}}, coords, nil)
 			var tree = hdb.NewHdb(2)
-			for i := range hulls {
-				tree.insert(hulls[i])
-			}
+			tree.Load(hulls)
 
 			var q = hulls[0]
 			var vs = FindNeighbours(tree, q.Geom, 0)
 			g.Assert(len(vs)).Equal(2)
-			vs = FindNodeNeighbours(tree, q, 0)
+			vs = FindNodeNeighbours(tree, &q, 0)
 			g.Assert(len(vs)).Equal(1)
 		})
 	})
